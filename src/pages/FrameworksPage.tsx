6f0d332ff409b4d2b18441121a08c4a6274e1b5f
rabotnik1 <randall@posteo.de>
2023-04-06 06:51:47 +1000

    added swift and hasura and spring logos

--- src/pages/FrameworksPage.tsx
+++ src/pages/FrameworksPage.tsx
@@ -6,7 +6,7 @@ interface FrameworksPageProps {}
 
 const FrameworksPage: FC<FrameworksPageProps> = () => {
   return (
-    <div className="page">
+    <div>
       <PageTitle title="frameworks" />
 
       <div className="flex-column align-center">
@@ -17,12 +17,20 @@ const FrameworksPage: FC<FrameworksPageProps> = () => {
           and dirty and implemented... Having worked extensively on numerous Flutter projects, I have deep theoretical and practical knowledge of the
           dart language and the flutter framework, including state management approaches, architecture and UI.
         </p>
-        <img src="./react-logo.png" alt="React.js logo" style={{ height: 70 }} />
-      <h2>react.js</h2>
+        <img src="./react_logo.svg" alt="React.js logo" style={{ height: 70 }} />
+        <h2>react.js</h2>
         <p>
           As a React enthusiast, I’ve watched React and its supporting libraries grow, and my knowledge is deep and broad. Through my own projects
           I’ve developed an array of both trusted and unusual solutions to logic and layout challenges of any project.
         </p>
+        <div className="grid">
+          <img src="./swift_logo.svg" alt="Swift logo" style={{ height: 60 }} />
+         <img src="./spring_logo.png" alt = "Spring Logo" style={{ height: 60 }}/>
+          <img src="./hasura_logo.svg" alt="Hasura logo" style={{ height: 60 }} />
+          <h2>swift</h2>
+          <h2>spring</h2>
+          <h2>hasura</h2>
+        </div>
       </div>
     </div>
   );
