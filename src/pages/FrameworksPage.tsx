5fb80e418c1c2956c987618272a5aa870d34217a
rabotnik1 <randall@posteo.de>
2023-04-06 07:23:28 +1000

    added more logos to projects age

--- src/pages/FrameworksPage.tsx
+++ src/pages/FrameworksPage.tsx
@@ -13,19 +13,18 @@ const FrameworksPage: FC<FrameworksPageProps> = () => {
         <img src="./flutter-logo.png" alt="" style={{ height: 60, paddingTop: 25 }} />
         <h2>flutter</h2>
         <p>
-          I've been a Flutter developer since early 2020. From humble beginnings I've come to know Flutter like the back of my hand. I've gotten down
-          and dirty and implemented... Having worked extensively on numerous Flutter projects, I have deep theoretical and practical knowledge of the
-          dart language and the flutter framework, including state management approaches, architecture and UI.
+          I've been developing Flutter apps since early 2020. Having worked extensively on numerous Flutter projects, I have deep theoretical and
+          practical knowledge of the dart language, the flutter framework, state management approaches, architecture and layout.
         </p>
         <img src="./react_logo.svg" alt="React.js logo" style={{ height: 70 }} />
         <h2>react.js</h2>
         <p>
-          As a React enthusiast, I’ve watched React and its supporting libraries grow, and my knowledge is deep and broad. Through my own projects
-          I’ve developed an array of both trusted and unusual solutions to logic and layout challenges of any project.
+          As a React developer, my knowledge is deep and broad. I’ve developed an array of both trusted and unusual solutions to logic and layout
+          challenges for a range of projects.
         </p>
         <div className="grid">
           <img src="./swift_logo.svg" alt="Swift logo" style={{ height: 60 }} />
-         <img src="./spring_logo.png" alt = "Spring Logo" style={{ height: 60 }}/>
+          <img src="./spring_logo.png" alt="Spring Logo" style={{ height: 60 }} />
           <img src="./hasura_logo.svg" alt="Hasura logo" style={{ height: 60 }} />
           <h2>swift</h2>
           <h2>spring</h2>
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
 import PageTitle from "../PageTitle";
+import "./../css/global.css";
 
 interface ProjectsPageProps {}
 
 const ProjectsPage: FC<ProjectsPageProps> = () => {
   return (
     <div className="page">
-      <PageTitle title="projects" />{" "}
+      <PageTitle title="projects" />
+      <div className="flex-column align-center center-text">
+        <img src="./psi_logo.svg" alt="Psi App Logo" />
+        <h2>test of telepathic abilities app</h2>
+        <img src="./wilde_party_logo.svg" alt=" Wilde Party Logo" />
+        <h2>browser card game</h2>
+      </div>
     </div>
   );
 };
